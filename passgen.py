8eae3b8d48296978208af56b08116748d45f98d4
NillasKA <kramernicklas@gmail.com>
2023-08-06 21:51:23 +0200

    First commit

---
+++ passgen.py
@@ -0,0 +1,18 @@
+#Nicklas' password generator
+import random
+
+#Variables
+letters = "abcdefghijklmnopqrstuvwxyz!@%&?#1234567890"
+pass_length = random.randrange(15, 20)
+
+#The generator itself
+def generator():
+    password = ""
+    for i in range(0, pass_length):
+        ran_index = random.randint(0, len(letters) - 1)
+        password += letters[ran_index]
+    return password
+
+generator()
+print("length of password:", pass_length, "letters")
+print(generator())
